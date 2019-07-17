87855b3b7fd3bad65458236e99ed44c20578f0a1
Evgeny Safronov <division494@gmail.com>
2019-07-17 13:50:21 +0300

    chore: spy interface prototyping

--- homekit/device/spy/mod.go
+++ homekit/device/spy/mod.go
@@ -3,15 +3,23 @@ package spy
 import (
 	"context"
 	"net"
+	"time"
 )
 
-type Spy struct {}
-
-func (m *Spy) Watch(ip net.IP, mac net.HardwareAddr, onActivity func()) uint64 {
-	return 0
+type Spy struct {
+	IdleTimeout time.Duration
 }
 
-func (m *Spy) Unwatch(id uint64) {}
+func (m *Spy) IsUp(mac net.HardwareAddr) bool {
+	return time.Now().Sub(m.HardwareLastSeen(mac)) < m.IdleTimeout
+}
+
+func (m *Spy) HardwareLastSeen(mac net.HardwareAddr) time.Time {
+	return time.Time{}
+}
+
+func (m *Spy) Register(mac net.HardwareAddr) {
+}
 
 func (m *Spy) Run(ctx context.Context) error {
 	return nil
